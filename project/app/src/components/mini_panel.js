76218ac9225d3256ae09796147fca8568bd72bf6
Hugo1307 <hugomiguel_2001_@sapo.pt>
2021-12-17 23:36:24 +0000

    Fix: Fixed broken dependencies and mini_panel layout.

--- project/app/src/components/mini_panel.js
+++ project/app/src/components/mini_panel.js
@@ -30,12 +30,12 @@ export class MiniPanel extends React.Component {
 
             <Card className={this.state.isActive ? '' + normalClasses : normalClasses} style={{borderRadius: "15px", width: "90%"}} onClick={this.handleClick.bind(this)}>
                 <Row className="my-2">
-                    <Col className="mx-2 justify-content-center d-flex w-50">
-                        <IconButton className="my-2 p-4" style={{borderRadius: "15px", backgroundColor: "#f76540"}}>
-                            <FontAwesomeIcon icon={this.props.icon} style={{fontSize: "2vw", minWidth: "40px"}}/>
+                    <Col className="col-md col-lg-3 offset-lg-1 text-center">
+                        <IconButton className="my-3 p-4" style={{borderRadius: "15px", backgroundColor: "#f76540"}}>
+                            <FontAwesomeIcon icon={this.props.icon} style={{fontSize: "115%", color: "white"}}/>
                         </IconButton>
                     </Col>
-                    <Col className="col mx-4 w-50" >
+                    <Col className="col-md col-lg mx-4" >
                         <p className="fs-8 fw-bolder mt-3 mb-0">Room</p>
                         <p className="fs-6 fw-normal mb-2">{this.props.room_name}</p>
                         <p className="fs-7 fw-light text-secondary">{this.props.power_consumption}</p>
